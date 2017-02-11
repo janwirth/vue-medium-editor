43fdd697ef44d15cdcdbad1128cf31d7e225ebd2
Jan Wirth <jan_wirth@lesz.cool>
2017-02-11 10:32:18 +0100

    Add configurability through curried functions

--- vue-medium-editor.js
+++ vue-medium-editor.js
@@ -9,35 +9,47 @@ function replaceElementWith (element, tagName) {
   return newElement
 }
 
-export default {
-  name: 'medium-editor',
-  props: ['text', 'customTag'],
-  template: '<div ref="element" v-html="text"> </div>',
+// http://stackoverflow.com/questions/6248666/how-to-generate-short-uid-like-ax4j9z-in-js
+function generateUIDNotMoreThan1million() {
+    return ("0000" + (Math.random()*Math.pow(36,4) << 0).toString(36)).slice(-4)
+}
 
-  mounted (evt) {
+export default function VueMediumEditor (options) {
+  const uniqueKey = generateUIDNotMoreThan1million()
+  return {
+    name: 'medium-editor',
+    props: ['text', 'customTag'],
+    template: '<div ref="element" v-html="text"> </div>',
+
+    mounted (evt) {
 // Use custom tag or div as editor element.
-    this.$refs.element = replaceElementWith(this.$refs.element, this.customTag || 'div')
-    this.$refs.element.innerHTML = this.text
+      this.$refs.element = replaceElementWith(this.$refs.element, this.customTag || 'div')
+      this.$refs.element.innerHTML = this.text
 
+// Init module
+// Here we store the instances with different configurations
+      this.$root.mediumEditor = this.$root.mediumEditor || {}
+
+      console.log(this.$root.mediumEditor[uniqueKey])
 // if Medium Editor is not instantiated yet, create a new instance
-    if (!this.$root.mediumEditor) {
-      this.$root.mediumEditor = new MediumEditor(this.$refs.element)
+      if (!this.$root.mediumEditor[uniqueKey]) {
+        this.$root.mediumEditor[uniqueKey] = new MediumEditor(this.$refs.element, options)
 
 // otherwise, just add the element
-    } else {
-      this.$root.mediumEditor.addElements(this.$refs.element)
-    }
+      } else {
+        this.$root.mediumEditor[uniqueKey].addElements(this.$refs.element)
+      }
 
 // bind edit operations to model
-    this.$refs.element.addEventListener('DOMSubtreeModified', () => {
-      if (this.$refs.element.childNodes[0]) {
-        this.$emit('edit', this.$refs.element.innerHTML)
-      }
-    })
-  },
+      this.$refs.element.addEventListener('DOMSubtreeModified', () => {
+        if (this.$refs.element.childNodes[0]) {
+          this.$emit('edit', this.$refs.element.innerHTML)
+        }
+      })
+    },
 
-  beforeDestroy (evt) {
-    this.$root.mediumEditor.removeElements(this.$refs.element)
+    beforeDestroy (evt) {
+      this.$root.mediumEditor.removeElements(this.$refs.element)
+    }
   }
-
 }
