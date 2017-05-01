a1f50b44e601e466bbcb474d67d76105e327dfcf
Jan Wirth <jottenwe@gmail.com>
2017-05-01 23:55:09 +0200

    Close #24 & close #22 Improve architecture & detach edit handler on destruction Also pass reference to API in event

--- vue-medium-editor.js
+++ vue-medium-editor.js
@@ -1,14 +1,5 @@
 import MediumEditor from 'medium-editor'
 
-// impure helper function, replaces some element from the dom with a new one of the given tag name
-// returns the new one.
-
-function replaceElementWith (element, tagName) {
-  let newElement = document.createElement(tagName)
-  element.parentNode.replaceChild(newElement, element)
-  return newElement
-}
-
 export default {
   name: 'medium-editor',
   props: {
@@ -16,15 +7,11 @@ export default {
     customTag: [String],
     options: {
       type: [Object],
-      default: function () {
-        return {}
-      }
+      default: () => {}
     },
     reuseMediumEditorInstance: {
       type: [Boolean],
-      default: function () {
-        return true
-      }
+      default: () => true
     }
   },
   data: function () {
@@ -32,57 +19,53 @@ export default {
       editor: null
     }
   },
-  template: '<div ref="element" v-html="text"> </div>',
+  render (h) {
+    return h( this.customTag || 'div', { ref: 'element' })
+  },
 
   mounted (evt) {
-    // Use custom tag or div as editor element.
-    this.$refs.element = replaceElementWith(this.$refs.element, this.customTag || 'div')
     this.$refs.element.innerHTML = this.text
 
     // If we want to reuse a single MediumEditor instance.
     if (this.reuseMediumEditorInstance) {
       // if Medium Editor is not instantiated yet, create a new instance
       if (!this.$root.mediumEditor) {
-        this.$root.mediumEditor = new MediumEditor(this.$refs.element, this.options)
+        this.api = this.$root.mediumEditor = new MediumEditor(this.$refs.element, this.options)
 
       // otherwise, just add the element
       } else {
-        this.$root.mediumEditor.addElements(this.$refs.element)
+        this.api = this.$root.mediumEditor
+        this.api.addElements(this.$refs.element)
       }
     // Otherwise create a new instance of MediumEditor to use.
     } else {
-      this.editor = new MediumEditor(this.$refs.element, this.options)
+      this.api = new MediumEditor(this.$refs.element, this.options)
     }
 
     // bind edit operations to model
-    this.$refs.element.addEventListener('DOMSubtreeModified', () => {
-      if (this.$refs.element.childNodes[0]) {
-        this.$emit('edit', this.$refs.element.innerHTML)
-      }
-    })
+    // we need to store the handler in order to later on detach it again
+    this.emit = event => this.$emit('edit', {event, api: this.api})
+    this.api.subscribe('editableInput', this.emit)
   },
 
   beforeDestroy (evt) {
     // Only try to remove our element from a shared MediumEditor instance if
     // we are using the shared instance.
+    this.api.unsubscribe('editableInput', this.emit)
     if (this.reuseMediumEditorInstance) {
-      this.$root.mediumEditor.removeElements(this.$refs.element)
+      this.api.removeElements(this.$refs.element)
     } else {
-      this.editor.destroy()
-      this.$refs.element.parentNode.removeChild(this.$refs.element)
+      this.api.destroy()
     }
   },
-
   watch: {
-    text() {
-      if (this.text === this.$refs.element.innerHTML) {
-        // if the change is done by this same component, do not update the contents to prevent
-        // caret from resetting to the beginning of the editor
-        return;
+    text( newText ) {
+      // innerHTML MUST not be performed if the text did not actually change.
+      // otherwise, the caret position will be reset.
+      if (newText !== this.$refs.element.innerHTML) {
+        this.$refs.element.innerHTML = this.text
       }
-      this.$refs.element.innerHTML = this.text
     }
   },
-
   MediumEditor
 }
