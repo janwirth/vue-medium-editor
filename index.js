863bc44ad6147a89636d1b08f88293afa8d5ed0b
dmattia <dmattia@nd.edu>
2017-07-19 18:21:30 -0500

    Allow watching for changes to editor options

--- index.js
+++ index.js
@@ -4,67 +4,59 @@ export default {
   name: 'medium-editor',
   props: {
     text: [String],
-    customTag: [String],
+    customTag: {
+      type: [String],
+      default: () => 'div'
+    },
     options: {
       type: [Object],
       default: () => {}
-    },
-    reuseMediumEditorInstance: {
-      type: [Boolean],
-      default: () => true
-    }
-  },
-  data: function () {
-    return {
-      editor: null
     }
   },
   render (h) {
-    return h( this.customTag || 'div', { ref: 'element' })
+    return h(this.customTag, { ref: 'element' })
   },
 
   mounted (evt) {
-    this.$refs.element.innerHTML = this.text
-
-    // If we want to reuse a single MediumEditor instance.
-    if (this.reuseMediumEditorInstance) {
-      // if Medium Editor is not instantiated yet, create a new instance
-      if (!this.$root.mediumEditor) {
-        this.api = this.$root.mediumEditor = new MediumEditor(this.$refs.element, this.options)
-
-      // otherwise, just add the element
-      } else {
-        this.api = this.$root.mediumEditor
-        this.api.addElements(this.$refs.element)
-      }
-    // Otherwise create a new instance of MediumEditor to use.
-    } else {
-      this.api = new MediumEditor(this.$refs.element, this.options)
-    }
-
-    // bind edit operations to model
-    // we need to store the handler in order to later on detach it again
-    this.emit = event => this.$emit('edit', {event, api: this.api})
-    this.api.subscribe('editableInput', this.emit)
+    this.createAndSubscribe()
   },
 
   beforeDestroy (evt) {
-    // Only try to remove our element from a shared MediumEditor instance if
-    // we are using the shared instance.
-    this.api.unsubscribe('editableInput', this.emit)
-    if (this.reuseMediumEditorInstance) {
-      this.api.removeElements(this.$refs.element)
-    } else {
+    this.tearDown()
+  },
+  methods: {
+    tearDown () {
+      this.api.unsubscribe('editableInput', this.emit)
       this.api.destroy()
+    },
+    createAndSubscribe () {
+      this.$refs.element.innerHTML = this.text
+
+      this.api = new MediumEditor(this.$refs.element, this.options)
+
+      // bind edit operations to model
+      // we need to store the handler in order to later on detach it again
+      this.emit = event => this.$emit('edit', {event, api: this.api})
+      this.api.subscribe('editableInput', this.emit)
     }
   },
   watch: {
-    text( newText ) {
+    text (newText) {
       // innerHTML MUST not be performed if the text did not actually change.
       // otherwise, the caret position will be reset.
       if (newText !== this.$refs.element.innerHTML) {
         this.$refs.element.innerHTML = this.text
       }
+    },
+    /**
+     * There is currently no way to change the options of a medium editor
+     * without destroying and re-setting up the MediumEditor object.
+     * See: https://github.com/yabwe/medium-editor/issues/1129
+     */
+    options (newOptions) {
+      console.log(this.api.serialize())
+      this.tearDown()
+      this.createAndSubscribe()
     }
   },
   MediumEditor
