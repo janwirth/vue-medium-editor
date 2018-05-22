5b41a7df622676074a358e98d7b73d60eb514da7
Jan Wirth <jottenwe@gmail.com>
2018-05-22 20:07:39 +0200

    Update playground

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,33 +1,38 @@
 <template>
   <div id="app">
-    <input type='checkbox' v-model='show'>
-    <input type='text' v-model='text'>
-    <div v-html='text'> </div>
-    <div> {{text}} </div>
-    <medium-editor
-      v-if='show'
-      :text='text'
-      v-on:edit='applyTextEdit'
-      custom-tag='h1'/>
-
-    <input type='checkbox' v-model='show'>
-    <input type='text' v-model='text'>
-    <div v-html='text'> </div>
-    <div> {{text}} </div>
+    <h1>Instance 1</h1>
     <medium-editor
       v-if='show'
       :text='text'
       :options='options'
       v-on:edit='applyTextEdit'
       custom-tag='pre'/>
+
+    <h2>Result</h2>
+    <div v-html='text'> </div>
+    <h2>Raw Result</h2>
+    <div> {{text}} </div>
+
+    <h2>Options</h2>
+    <textarea
+      :value='JSON.stringify(options, null, 2)'
+      v-on:keyup='applyOptions'
+    ></textarea>
+    <pre v-if='!optionsValid'> Options don't parse! </pre>
+    <h2> Visibility</h2>
+    <input type='checkbox' v-model='show'>
+    <h2>As Text input</h2>
+    <input type='text' v-model='text'>
+    </br>
   </div>
 </template>
 
 <script>
 import MediumEditor from '../../index.js'
 
-const text = 'Hello World'
+const text = 'hello world'
 
+const optionsValid = true
 const show = true
 const tag = 'h1'
 const options = {
@@ -48,8 +53,17 @@ const options = {
 
 export default {
   name: 'app',
-  data: () => ({text, show, tag, options}),
+  data: () => ({text, show, tag, options, optionsValid}),
   methods: {
+    applyOptions (ev) {
+      console.log(ev)
+      try {
+        this.options = JSON.parse(ev.target.value)
+        this.optionsValid = true
+      } catch (e) {
+        this.optionsValid = false
+      }
+    },
     applyTextEdit (ev) {
       console.log(ev)
       if (ev.event.target) {
@@ -71,4 +85,9 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+textarea {
+  width: 600px;
+  height: 500px;
+}
 </style>
+
